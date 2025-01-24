eeee8695898aec5dfc4ec1751439734a2523ec24
Nadine AHOMAGNON <nadineahomagnon@gmail.com>
2025-01-24 13:07:06 +0100

    choix de la version

--- chatbotgpt.py
+++ chatbotgpt.py
@@ -41,3 +41,14 @@ if prompt := st.chat_input("What is up?"):
         )
         response = st.write_stream(stream)
     st.session_state.messages.append({"role": "assistant", "content": response})
+    
+    import streamlit as st
+
+# Sélection du modèle GPT
+model_type = st.selectbox(
+    "Choisissez un type de modèle GPT :",
+    ["gpt-3.5-turbo", "instruction gpt-3.5-turbo", "gpt-3.5-turbo-1106", "gpt-3.5-turbo-0125"]
+)
+
+# Affichage du modèle sélectionné
+st.write(f"Modèle sélectionné : {model_type}")
